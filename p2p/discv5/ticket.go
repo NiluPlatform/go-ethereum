e2f48feec15dd547f7231174f98341b5f37f0b2c
Péter Szilágyi <peterke@gmail.com>
2017-12-28 15:17:03 +0200

    p2p/discv5: fix reg lookup, polish code, use logger (#15737)

--- p2p/discv5/ticket.go
+++ p2p/discv5/ticket.go
@@ -25,10 +25,10 @@ import (
 	"sort"
 	"time"
 
-	"github.com/NiluPlatform/go-nilu/common"
-	"github.com/NiluPlatform/go-nilu/common/mclock"
-	"github.com/NiluPlatform/go-nilu/crypto"
-	"github.com/NiluPlatform/go-nilu/log"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/mclock"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 const (
