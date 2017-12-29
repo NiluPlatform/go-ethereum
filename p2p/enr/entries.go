94cc498ca5537285716c2096be4a33babae8a45f
Anton Evangelatov <anton.evangelatov@gmail.com>
2017-12-29 21:18:51 +0100

    p2p/enr: initial implementation (#15585)
    Initial implementation of ENR according to ethereum/EIPs#778

--- p2p/enr/entries.go
+++ p2p/enr/entries.go
@@ -22,8 +22,8 @@ import (
 	"io"
 	"net"
 
-	"github.com/NiluPlatform/go-nilu/crypto"
-	"github.com/NiluPlatform/go-nilu/rlp"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 // Entry is implemented by known node record entry types.
