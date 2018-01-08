5cb7c6d7c1534debc03119c41dfcf2a09709f3bb
Felix Lange <fjl@users.noreply.github.com>
2018-01-08 13:15:57 +0100

    all: update generated code (#15808)
    * core/types, core/vm, eth, tests: regenerate gencodec files
    
    * Makefile: update devtools target
    
    Install protoc-gen-go and print reminders about npm, solc and protoc.
    Also switch to github.com/kevinburke/go-bindata because it's more
    maintained.
    
    * contracts/ens: update contracts and regenerate with solidity v0.4.19
    
    The newer upstream version of the FIFSRegistrar contract doesn't set the
    resolver anymore. The resolver is now deployed separately.
    
    * contracts/release: regenerate with solidity v0.4.19
    
    * contracts/chequebook: fix fallback and regenerate with solidity v0.4.19
    
    The contract didn't have a fallback function, payments would be rejected
    when compiled with newer solidity. References to 'mortal' and 'owned'
    use the local file system so we can compile without network access.
    
    * p2p/discv5: regenerate with recent stringer
    
    * cmd/faucet: regenerate
    
    * dashboard: regenerate
    
    * eth/tracers: regenerate
    
    * internal/jsre/deps: regenerate
    
    * dashboard: avoid sed -i because it's not portable
    
    * accounts/usbwallet/internal/trezor: fix go generate warnings

--- contracts/ens/contract/publicresolver.go
+++ contracts/ens/contract/publicresolver.go
@@ -7,10 +7,10 @@ import (
 	"math/big"
 	"strings"
 
-	"github.com/NiluPlatform/go-nilu/accounts/abi"
-	"github.com/NiluPlatform/go-nilu/accounts/abi/bind"
-	"github.com/NiluPlatform/go-nilu/common"
-	"github.com/NiluPlatform/go-nilu/core/types"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 // PublicResolverABI is the input ABI used to generate the binding from.
